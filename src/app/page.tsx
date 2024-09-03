4ecc3e939b68204b031d29c1a8d0f717aca27136
3b00d09 <bd315977@gmail.com>
2024-09-04 00:42:28 +0100

    feat: adjusted font size for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Button from "@/lib/components/Button"
 
 export default function page() {
     return (
-        <main className="h-screen">
-            <h1 className="text-primary text-center mouse-memoirs-regular text-[5rem]">Who's Out</h1>
-            <div className="flex items-center justify-center w-full flex-col gap-2 mt-10">
+        <main className="h-screen flex flex-col gap-32">
+            <h1 className="text-primary text-center mouse-memoirs-regular text-[10rem]">Who's Out</h1>
+            <div className="flex items-center justify-center w-full flex-col gap-2 text-[3rem]">
                 <div className="grid grid-cols-2 gap-2">
                     <Button text="Create Lobby"/>
                     <Button text="Join Lobby"/>
