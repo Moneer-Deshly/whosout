e9f07cf27b1d49bce6bc8aaa902609cf406beff4
3b00d09 <bd315977@gmail.com>
2024-09-09 15:09:05 +0100

    feat: lobbies now stored in redis global cache

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import { createServer } from "node:http";
 import next from "next";
 import { Server } from "socket.io";
+import { createClient } from 'redis';
+import 'dotenv/config'
+
+const redisClient = createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: 'redis-15667.c338.eu-west-2-1.ec2.redns.redis-cloud.com',
+        port: 15667
+    }
+});
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
@@ -9,41 +19,53 @@ const port = 3000;
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
-// <lobbyKey:players[]>
-// can use sets in the future if usernames cant be duped
+// struct of lobbies
+// map<lobbyId, {creator: string, players:[]}>
 
-const lobbies = new Map()
-
-app.prepare().then(() => {
+app.prepare().then(async () => {
   const httpServer = createServer(handler);
 
   const io = new Server(httpServer);
+  
+  await redisClient.connect();
 
-  io.on("connection", (socket) => {
 
-    socket.on("create", (val)=>{
-      if(!lobbies.get(val.lobbyId)){
-        lobbies.set(val.lobbyId, [val.username])
-      }
-      else{
-        const players = lobbies.get(val.lobbyId)
-        players.push(val.username)
-        lobbies.set(val.lobbyId, players)
-      }
+  io.on("connection", async(socket) => {
+    socket.on("create", async (val)=>{
+      // set a new map in our redis db
+      // players must be stringified because arrays are an invalid data type
+      await redisClient.hSet(
+        val.lobbyId,
+        {
+          'creator': val.username,
+          'players': JSON.stringify(["p1", "p2", "p3"]),
+        }
+      )
 
-      io.emit("lobbiesUpdate", Array.from(lobbies));
+      //io.emit("lobbiesUpdate", Array.from(lobbies));
     })
 
-    socket.on("join", (val)=>{
-      if(!lobbies.get(val.lobbyId)){
+    socket.on("join", async(val)=>{
+      const exists = await redisClient.exists(val.lobbyId)
+      if(!exists){
         socket.emit("error", "invalid lobby code")
+        return;
       }
+
       else{
-        const players = lobbies.get(val.lobbyId);
-        players.push(val.username);
-        lobbies.set(val.lobbyId, players);
+        // parse the stringified players array from the map
+        const players = JSON.parse(await redisClient.hGet(val.lobbyId, "players"))
+
+        // using hSet with an existing key just updates the map with the values we provide
+        // creator wont change here as its not a provided field
+        await redisClient.hSet(
+          val.lobbyId,
+          {
+            'players': JSON.stringify([...players, val.username]),
+          }
+        )
       }
-      io.emit("lobbiesUpdate", Array.from(lobbies));
+      //io.emit("lobbiesUpdate", Array.from(lobbies));
     })
 
 
